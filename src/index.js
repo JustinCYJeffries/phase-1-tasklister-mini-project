94b17650bd86b94bcadc5007036003e27089d585
Justin Jeffries <justincyjeffries@gmail.com>
2022-02-13 17:50:50 -0700

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
+
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  document.querySelector('form').addEventListener('submit', (e) => {
+   e.preventDefault() 
+   let newTaskDescription = document.querySelector('#new-task-description').value
+   let list = document.createElement('li')
+   let btn = document.createElement('button')
+   btn.textContent = 'x'
+   list.textContent = `${newTaskDescription}`
+   list.appendChild(btn)
+   document.querySelector('#tasks').appendChild(list)
+   btn.addEventListener('click' , () => list.remove())
+  })
+})
+
